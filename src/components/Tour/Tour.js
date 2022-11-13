ea4e03fc0fb3cbc745dc594ecd779daa8dc0746d
msebarjr <msebarjr@gmail.com>
2022-11-13 14:53:56 -0500

    Added responsiveness to video

--- src/components/Tour/Tour.js
+++ src/components/Tour/Tour.js
@@ -4,6 +4,8 @@ function Tour() {
     return (
         <div className="video">
             <iframe
+                // width="1280"
+                // height="702"
                 src="https://www.youtube.com/embed/6ZGv3zXMWBg"
                 title="Tour of Home"
                 frameborder="0"
