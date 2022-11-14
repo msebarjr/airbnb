f745c5dff2f88af0a1674f01e6829e815eb4f0f8
msebarjr <msebarjr@gmail.com>
2022-11-14 15:04:21 -0500

    Switched video to vimeo

--- src/components/Tour/Tour.js
+++ src/components/Tour/Tour.js
@@ -6,13 +6,23 @@ function Tour() {
             <iframe
                 // width="1280"
                 // height="702"
-                src="https://www.youtube.com/embed/6ZGv3zXMWBg"
+                // src="https://www.youtube.com/embed/6ZGv3zXMWBg"
+                src="https://player.vimeo.com/video/764003141?h=53a9dcc539&badge=0&autopause=0&player_id=0&app_id=58479/embed"
                 title="Tour of Home"
                 frameborder="0"
                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                 allowfullscreen={true}
             ></iframe>
         </div>
+        // <div className="vimeo">
+        //     <iframe
+        //         src="https://player.vimeo.com/video/764003141?h=53a9dcc539&badge=0&autopause=0&player_id=0&app_id=58479/embed"
+        //         allow="autoplay; fullscreen; picture-in-picture"
+        //         allowfullscreen
+        //         frameborder="0"
+        //         title="Tour of home"
+        //     ></iframe>
+        // </div>
     );
 }
 
